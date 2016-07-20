7829a3b462d87b5b2b7cc519d75054feceba1cbc
Phillip Jones <phillipemersonjones@gmail.com>
2016-07-20 02:19:25 -0500

    nodemon setup to restart server when public files change

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-var express = require('express');
+const express = require('express'),
+      app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000.');
+});
