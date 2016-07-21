07996ee3b93d182ff67130727f66f21188635938
Phillip Jones <phillipemersonjones@gmail.com>
2016-07-21 18:27:04 -0500

    create todo- C of CRUD

--- routes/api.js
+++ routes/api.js
@@ -5,10 +5,22 @@ const express = require('express'),
       Todo = require('../models/todos');
 
 
-router.post('/todos/all', (req, res) => {
+router.get('/todos/all', (req, res) => {
   Todo.find({
   },(err, docs) => {
     res.send(docs);
+  });
+});
+
+router.put('/todos/create', (req, res) => {
+  console.log(req.body);
+  var todo = new Todo({todo: req.body.todo, user: mongoose.Types.ObjectId( req.body.user )});
+  todo.save((err, doc) => {
+    if (err) return console.log(err);
+    res.send({
+        msg:"state successul!",
+        data:doc
+    });
   });
 });
 
