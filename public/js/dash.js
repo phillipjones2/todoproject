be8c3348e3ee14711a024037f86ff077e9f58168
Colton Colcleasure <colshacol@gmail.com>
2016-07-20 21:20:35 -0500

    Created dash.js + dash.pug + dash.scss

---
+++ public/js/dash.js
@@ -0,0 +1,58 @@
+'use strict';
+
+console.clear();
+var todoContainer = document.getElementById('todo-container'),
+    newTodoContainer = document.getElementById('new-todo-container'),
+    todos = document.getElementById('todos');
+
+// Mock JSON data to receive from server.
+var serverResponse = {
+  user: {
+    name: 'Colton'
+  },
+
+  data: [{
+    title: 'title one',
+    content: 'content one',
+    date: '12/12/2012'
+  }, {
+    title: 'title two',
+    content: 'content two',
+    date: '12/12/2020'
+  }]
+};
+
+// Insert the user's name into the #user-name span.
+document.getElementById('user-name').innerText = serverResponse.user.name;
+
+// For each todo objct returned from the server...
+serverResponse.data.forEach(function (todo, i) {
+  // Create a DOM element.
+  var todoElement = document.createElement('div');
+  // Give it an ID and classes.
+  todoElement.setAttribute('id', 'todo-' + i);
+  todoElement.setAttribute('class', 'todo');
+
+  // Create elements that will make up the todo in the document.
+  var titleElement = document.createElement('h3'),
+      contentElement = document.createElement('p'),
+      dateElement = document.createElement('p');
+
+  // Create text nodes to be inserted into respective elements.
+  var todoTitle = document.createTextNode(todo.title),
+      todoContent = document.createTextNode(todo.content),
+      todoDate = document.createTextNode(todo.date);
+
+  // Push text nodes into respective elements.
+  titleElement.appendChild(todoTitle);
+  contentElement.appendChild(todoContent);
+  dateElement.appendChild(todoDate);
+
+  // Place those elements inside of the todoElement.
+  todoElement.appendChild(titleElement);
+  todoElement.appendChild(contentElement);
+  todoElement.appendChild(dateElement);
+
+  // Add the todoElement into #todo-container.
+  todoContainer.appendChild(todoElement);
+});
