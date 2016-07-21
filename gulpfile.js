8b00e52ed72184cf9fe3b7c7f692f6de5581eb9d
Alexg2195 <alexg2195@gmail.com>
2016-07-21 14:00:54 -0500

    Added livereload to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,23 @@ const gulp = require('gulp'),
       pug  = require('gulp-pug'),
       sass = require('gulp-sass'),
       babel = require('gulp-babel'),
-      watch = require('gulp-watch');
+      watch = require('gulp-watch'),
+      livereload = require('gulp-livereload');
 
 gulp.task('pug-compile', () => {
   return gulp.src('./src/views/*.pug')
     .pipe(pug({
       pretty: true,
     }))
-    .pipe(gulp.dest('./public/'));
+    .pipe(gulp.dest('./public/'))
+    .pipe(livereload());
 });
 
 gulp.task('sass-compile', () => {
   return gulp.src('./src/styles/main.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./public/css/'));
+    .pipe(gulp.dest('./public/css/'))
+    .pipe(livereload());
 });
 
 gulp.task('babel-compile', () => {
@@ -23,10 +26,12 @@ gulp.task('babel-compile', () => {
     .pipe(babel({
       presets: ['es2015'],
     }))
-    .pipe(gulp.dest('./public/js/'));
+    .pipe(gulp.dest('./public/js/'))
+    .pipe(livereload());
 });
 
 gulp.task('watch', () => {
+  livereload.listen();
   gulp.watch('./src/scripts/*.js', ['babel-compile']);
   gulp.watch('./src/styles/*.scss', ['sass-compile']);
   gulp.watch('./src/views/*.pug', ['pug-compile']);
