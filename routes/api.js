74f50433d2081d1f6c5847339aabca5f5935b58f
Phillip Jones <phillipemersonjones@gmail.com>
2016-07-21 18:49:34 -0500

    Put end point is working- U of CRUD

--- routes/api.js
+++ routes/api.js
@@ -12,7 +12,7 @@ router.get('/todos/all', (req, res) => {
   });
 });
 
-router.put('/todos/create', (req, res) => {
+router.post('/todos', (req, res) => {
   console.log(req.body);
   var todo = new Todo({todo: req.body.todo, user: mongoose.Types.ObjectId( req.body.user )});
   todo.save((err, doc) => {
@@ -22,6 +22,24 @@ router.put('/todos/create', (req, res) => {
         data:doc
     });
   });
+});
+
+router.put('/todos/:todo_id', (req, res) => {
+  // console.log(req.body);
+  Todo.findById(req.params.todo_id, (err, todo) => {
+    if (err)
+      res.send(err);
+    // console.log(`todo: ${todo.todo}`)
+    // console.log(`body todo: ${req.body.todo}`)
+    todo.todo = req.body.todo;
+    todo.save((err, doc) => {
+      if (err) return console.log(err);
+      res.send({
+          msg:"state successul!",
+          data:doc
+      });
+    });
+  })
 });
 
 
